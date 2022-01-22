22093847b695b45a64c6a83a4bec6db9e64110f9
mastheusum <victor.matheus.xd@gmail.com>
2022-01-22 16:42:45 -0300

    Rotate and WrapBehavior

---
+++ Assets/Scripts/Wrap.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wrap : MonoBehaviour
+{
+    public Camera cam;
+    private float leftConstant;
+    private float rightConstant;
+    private float topConstant;
+    private float bottomConstant;
+    float buffer = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rightConstant = cam.ScreenToWorldPoint(new Vector3(0, 0, cam.transform.position.z)).x;
+        leftConstant = cam.ScreenToWorldPoint(new Vector3(Screen.width, 0, cam.transform.position.z)).x;
+        bottomConstant = cam.ScreenToWorldPoint(new Vector3(0, 0, cam.transform.position.z)).y;
+        topConstant = cam.ScreenToWorldPoint(new Vector3(0, Screen.height, cam.transform.position.z)).y;
+        print("right: " + rightConstant);
+        print("left: " + leftConstant);
+        print( transform.position.y );
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (transform.position.y > topConstant) {
+            transform.position = new Vector3(
+                transform.position.x,
+                bottomConstant + buffer,
+                transform.position.z
+                );
+        }
+        else if (transform.position.y < bottomConstant) {
+            transform.position = new Vector3(
+                transform.position.x,
+                topConstant - buffer,
+                transform.position.z
+                );
+        }
+        if (transform.position.x > leftConstant) {
+            transform.position = new Vector3(
+                rightConstant + buffer,
+                transform.position.y,
+                transform.position.z
+                );
+        }
+        else if (transform.position.y < rightConstant) {
+            transform.position = new Vector3(
+                leftConstant - buffer,
+                transform.position.y,
+                transform.position.z
+                );
+        }        
+    }
+}
